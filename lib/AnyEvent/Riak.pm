ec1e256bf94d16108f673c17e5e28c24fe225dc4
franck cuny <franck@lumberjaph.net>
2009-12-10 14:22:43 +0100

    initial commit

---
+++ lib/AnyEvent/Riak.pm
@@ -0,0 +1,32 @@
+package AnyEvent::Riak;
+
+use Moose;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+AnyEvent::Riak -
+
+=head1 SYNOPSIS
+
+  use AnyEvent::Riak;
+
+=head1 DESCRIPTION
+
+AnyEvent::Riak is
+
+=head1 AUTHOR
+
+franck cuny E<lt>franck.cuny {at} rtgi.frE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
---
+++ xt/run.t
@@ -0,0 +1,4 @@
+use lib 'xt/tests';
+use Test::AnyEvent::Riak;
+
+Test::Class->runtests;
